adec7d6d9737cc50ba2171949808183059806675
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-01-08 19:10:46 +0600

    all added

--- ph.js
+++ ph.js
@@ -134,7 +134,7 @@ function factorial(num) {
     let result = 1;
     
     for (let i = 1; i <= num; i++) {
-        result *= i; // Multiply result by each number
+        result *= i;
     }
     
     return result;
