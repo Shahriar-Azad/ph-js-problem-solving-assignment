22910bc17a367511275931506ad143972c7c50fc
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-01-08 17:12:25 +0600

    capitalize first letter of a word

--- ph.js
+++ ph.js
@@ -113,3 +113,19 @@ function findEvenNumbers(arr) {
 
 console.log(findEvenNumbers([1, 2, 3, 4, 5, 6]));
 
+// 8. Capitalize First Letter of Each Word
+function capitalizeWords(str) {
+    let words = str.split(" ");
+    let result = [];
+    
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        let capitalized = word[0].toUpperCase() + word.slice(1);
+        result.push(capitalized);
+    }
+    
+    return result.join(" ");
+}
+
+console.log(capitalizeWords("hello world"));
+
