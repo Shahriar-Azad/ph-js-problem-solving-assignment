c3122c05833b23dbf4ad5d34da19b0f625bd89ce
Shahriar-Azad <shahriar.azad123@gmail.com>
2026-01-08 17:06:27 +0600

    reverse a string

---
+++ ph.js
@@ -0,0 +1,14 @@
+// 1. Reverse a String
+function stringReverse(str){
+    let reversedString = "";
+    for (let i = str.length - 1; i >=0; i--){
+        reversedString += str[i];
+
+    }
+    return reversedString;
+    
+}
+
+console.log(stringReverse("hello")) 
+
+
